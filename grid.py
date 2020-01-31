f573614a6aa42df0c49f64d96c8c4917c0a6b260
Erik Ring-Walters <erik.ring.walters@gmail.com>
2020-01-30 17:02:43 -0800

    boxes on grid don't overlap as much

--- grid.py
+++ grid.py
@@ -17,10 +17,10 @@ thickness = 4
 
 for i in range(0,3):
     for j in range(0,3):
-        startx = int((dim[0]) * ((i)/3))# - int(thickness/2)
-        starty = int((dim[1]) * ((j)/3))# - int(thickness/2)
-        endx = int((dim[0]) * ((i + 1)/3))# - int(thickness/2)
-        endy = int((dim[1]) * ((j + 1)/3))# - int(thickness/2)
+        startx = int((dim[0]) * ((i)/3)) + int(thickness/2)
+        starty = int((dim[1]) * ((j)/3)) + int(thickness/2)
+        endx = int((dim[0]) * ((i + 1)/3)) - int(thickness/2)
+        endy = int((dim[1]) * ((j + 1)/3)) - int(thickness/2)
         start = (startx, starty)
         end = (endx,endy)
         color = (i*75, 0, j*75)
